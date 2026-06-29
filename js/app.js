e4cb7a015402dbe0c2ce6288b0e22a138dcd014a
his-imag8 <ekanemdaniel60@gmail.com>
2026-06-29 23:53:31 +0100

    Refactored code

--- js/app.js
+++ js/app.js
@@ -5,12 +5,21 @@ const API_KEY = "5a7e69c75d29626271fba5012cefe52b";
 const modeBtn = document.querySelectorAll(".nav-mode-btn");
 const movieCard = document.querySelectorAll(".movies-grid-items");
 const movieContainer = document.querySelector(".movies-grid");
+const master = document.querySelector(".master");
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTdlNjljNzVkMjk2MjYyNzFmYmE1MDEyY2VmZTUyYiIsIm5iZiI6MTc3ODkzODc0OS43OTYsInN1YiI6IjZhMDg3MzdkYmQ3YWZlZTI3ZTUxOWZmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DQRzIPkZURQf_uhgjgWVBTfU8cTdlVObneRn6hh9UhY",
+  },
+};
 
 let maxNumberInPage;
 let currentPage = 1;
-
 let initialCount = 0;
 let totalCount = 5;
+let mode = "movie";
 
 /////////////////////////////
 // TOGGLE MODE BUTTON
@@ -24,31 +33,28 @@ modeBtn.forEach((mBtn) => {
 
     // console.log(e.target);
     e.target.classList.add("nav-mode-btn--active");
+
+    mode = e.target.dataset.mode;
+    console.log(mode);
   });
 });
 
-movieContainer.addEventListener("click", function (e) {
-  if (!e.target.classList.contains === "movies-grid-items") return;
+function activeClasses() {
+  movieContainer.addEventListener("click", function (e) {
+    if (!e.target.classList.contains === "movies-grid-items") return;
 
-  movieCard.forEach((card) => {
-    card.classList.remove("card--active");
+    movieCard.forEach((card) => {
+      card.classList.add("card--active");
+      console.log(card);
+    });
+
+    const closest = e.target.closest(".movies-grid-items");
+    closest.classList.add("card--active");
   });
-
-  const closest = e.target.closest(".movies-grid-items");
-  closest.classList.add("card--active");
-});
+}
 
 async function apiCall() {
   loaderSpinner6();
-
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTdlNjljNzVkMjk2MjYyNzFmYmE1MDEyY2VmZTUyYiIsIm5iZiI6MTc3ODkzODc0OS43OTYsInN1YiI6IjZhMDg3MzdkYmQ3YWZlZTI3ZTUxOWZmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DQRzIPkZURQf_uhgjgWVBTfU8cTdlVObneRn6hh9UhY",
-    },
-  };
 
   try {
     const res = await fetch(
@@ -57,35 +63,13 @@ async function apiCall() {
     );
     const data = await res.json();
     const { results } = data;
-
     maxNumberInPage = results.length - 1;
-
     const result = results.slice(initialCount, totalCount);
+
+    DisplayTrending(result);
+
     console.log(result);
     console.log(results);
-
-    const html = result
-      .map((data) => {
-        return `<div class="movies-grid-items">
-              <div class="movies-grid-items-container">
-                <img
-                  class="movies-grid-items-img"
-                  src="https://image.tmdb.org/t/p/w500/${data.poster_path}"
-                  alt="movie name"
-                />
-              </div>
-              <!-- src="https://tmdb.org${data.poster_path}" -->
-
-              <div class="movies-grid-items-info">
-                <h3 class="movies-grid-items-info-title">${data.original_title}</h3>
-                <p class="movies-grid-items-info-year">${data.release_date}</p>
-              </div>
-            </div>`;
-      })
-      .join("");
-
-    movieContainer.innerHTML = "";
-    movieContainer.insertAdjacentHTML("beforeend", html);
 
     console.log(data);
   } catch (err) {
@@ -115,15 +99,6 @@ function loaderSpinner6() {
   movieContainer.insertAdjacentHTML("afterbegin", loader);
 }
 
-const controlbtnHTML = `<div class="movies-btn">
-              <button class="movies-grid-btn movies-btn--previous">
-                <span>&larr;</span> previous
-              </button>
-              <button class="movies-grid-btn movies-btn--next">
-                next <span>&rarr;</span>
-              </button>
-            </div>`;
-
 /////////////////////
 // EVENTLISTENERS
 
@@ -132,34 +107,73 @@ function activeControls() {
   const btnPrevious = document.querySelector(".movies-btn--previous");
   btnPrevious.classList.add("hidden");
 
-  btnPrevious.addEventListener("click", function (e) {
-    e.preventDefault();
+  btnPrevious.addEventListener("click", previousAction);
 
-    if (initialCount === 0 && totalCount == 8) {
+  btnNext.addEventListener("click", nextAction);
+
+  function previousAction(e) {
+    e.preventDefault();
+    master.scrollIntoView({
+      behaviour: "smooth",
+    });
+
+    if (initialCount === 0 && totalCount == 8)
       return btnPrevious.classList.add("hidden");
-    } else {
-      initialCount -= 8;
-      totalCount -= 8;
-      apiCall();
-      console.log(initialCount, totalCount);
-    }
-  });
 
-  btnNext.addEventListener("click", function (e) {
+    if (currentPage > 1 && initialCount === 0) {
+      currentPage--;
+      initialCount = maxNumberInPage - 4;
+      totalCount = maxNumberInPage + 1;
+      console.log(initialCount, totalCount);
+      return apiCall();
+    }
+
+    initialCount -= 5;
+    totalCount -= 5;
+  }
+  function nextAction(e) {
     e.preventDefault();
+    master.scrollIntoView({
+      behaviour: "smooth",
+    });
 
     if (totalCount >= maxNumberInPage) {
       currentPage++;
 
-      initialCount = -8;
+      initialCount = -5;
       totalCount = 0;
     }
     btnPrevious.classList.remove("hidden");
-    initialCount += 8;
-    totalCount += 8;
+    initialCount += 5;
+    totalCount += 5;
     apiCall();
-    console.log(initialCount, totalCount);
-  });
+  }
 }
-
 activeControls();
+
+function DisplayTrending(inputData) {
+  const html = inputData
+    .map((data) => {
+      return `<div class="movies-grid-items" href="${data.id}">
+              <div class="movies-grid-items-container">
+                <img
+                  class="movies-grid-items-img"
+                  src="https://image.tmdb.org/t/p/w500/${data.poster_path}"
+                  alt="movie name"
+                />
+              </div>
+              <!-- src="https://tmdb.org${data.poster_path}" -->
+
+              <div class="movies-grid-items-info">
+                <h3 class="movies-grid-items-info-title">${data.original_title}</h3>
+                <p class="movies-grid-items-info-year">${data.release_date}</p>
+              </div>
+            </div>`;
+    })
+    .join("");
+
+  movieContainer.innerHTML = "";
+  movieContainer.insertAdjacentHTML("beforeend", html);
+
+  activeClasses();
+}
