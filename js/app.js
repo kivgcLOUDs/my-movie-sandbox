834df671e98bcdb84cf2675ac5c6434c0250694e
his-imag8 <ekanemdaniel60@gmail.com>
2026-06-30 21:58:26 +0100

    OOP refaturing starting

--- js/app.js
+++ js/app.js
@@ -26,6 +26,210 @@ let totalCount = 5;
 let mode = "movie";
 const newUrlParams = new URLSearchParams(window.location.search);
 
+class App {
+  maxNumberInPage;
+  currentPage = Math.trunc(Math.random() * 20);
+  initialCount = 0;
+  totalCount = 5;
+  mode = "movie";
+  newUrlParams = new URLSearchParams(window.location.search);
+
+  trendingAPI = `https://api.themoviedb.org/3/trending/movie/day?language=en-US&page=${currentPage}`;
+  searchAPI = `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=${currentPage}`;
+  urlAPI = this.trendingAPI;
+
+  constructor() {
+    this.apiCall(this.urlAPI);
+    this.activeControls();
+    masterBtn.addEventListener("click", this.searchBtnClick).bind(this);
+  }
+
+  async apiCall(url) {
+    this.loaderSpinner6();
+
+    try {
+      const res = await fetch(this.urlAPI, options);
+      const data = await res.json();
+      const { results } = data;
+      this.maxNumberInPage = results.length - 1;
+      const result = results.slice(this.initialCount, this.totalCount);
+
+      this.DisplayTrending(result);
+
+      console.log(result);
+      console.log(results);
+
+      console.log(data);
+    } catch (err) {
+      // movieContainer.innerHTML = ``;
+      // movieContainer.insertAdjacentText = `An error ocurred: ${err.message}`;
+      console.error(err.message);
+    }
+  }
+
+  //////////////////
+  // SEARCH CONTROLS
+  async DisplaySearchOutput(movieName) {
+    loaderSpinner6();
+    currentPage = 1;
+    this.initialCount = 0;
+
+    this.urlAPI = this.searchAPI;
+
+    try {
+      const res = await fetch(urlAPI, options);
+      if (!res.ok) throw new Error();
+      const data = await res.json();
+      const { results } = data;
+
+      // ////////////////
+      // // BASIC RESET FOR LOOPING ALGORITHM
+      // this.initialCount = 0;
+      // this.totalCount = 5;
+
+      this.maxNumberInPage = results.length - 1;
+      const result = results.slice(this.initialCount, this.totalCount);
+
+      this.DisplayTrending(result);
+      resultText.textContent = `Found Results`;
+
+      console.log(data);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  ///////////////
+  //DISPLAY TRENDING MOVIES
+  DisplayTrending(loopedData) {
+    const html = loopedData
+      .map((data) => {
+        return `<div class="movies-grid-items" data-id="${data.id}">
+              <div class="movies-grid-items-container">
+                <img
+                  class="movies-grid-items-img"
+                  src="https://image.tmdb.org/t/p/w500/${data.poster_path}"
+                  alt="${data.original_title}"
+                />
+              </div>
+              <!-- src="https://tmdb.org${data.poster_path}" -->
+
+              <div class="movies-grid-items-info">
+                <h3 class="movies-grid-items-info-title">${data.original_title}</h3>
+                <p class="movies-grid-items-info-year">${data.release_date}</p>
+              </div>
+            </div>`;
+      })
+      .join("");
+
+    movieContainer.innerHTML = "";
+    movieContainer.insertAdjacentHTML("beforeend", html);
+
+    this.activeCard();
+  }
+
+  activeCard() {
+    movieContainer.addEventListener("click", function (e) {
+      if (!e.target.classList.contains === "movies-grid-items") return;
+
+      movieCard.forEach((card) => {
+        card.classList.add("card--active");
+        console.log(card);
+      });
+
+      const closest = e.target.closest(".movies-grid-items");
+      closest.classList.add("card--active");
+
+      ////////////////
+      // FOR SWITCCHING TO DETAILED VIEW
+      const id = closest.dataset.id;
+      window.location.href = `#${id}`;
+      /////////////////
+    });
+  }
+
+  searchBtnClick(e) {
+    e.preventDefault();
+    const inputValue = masterInput.value;
+    this.totalCount = 8;
+    DisplaySearchOutput(inputValue);
+  }
+
+  ///////////////////
+  // LOADER SPINNER
+  loaderSpinner6() {
+    const loader = `
+ <div class="loader">
+            <svg class="loader-spinner">
+              <use xlink:href="img/sprite.svg#icon-spinner6"></use>
+            </svg>
+          </div>`;
+
+    ////////////////
+    // ADDING LOADER
+    movieContainer.innerHTML = "";
+    movieContainer.insertAdjacentHTML("afterbegin", loader);
+  }
+
+  /////////////////
+  // "SEE MORE CONTROLS FOR THE HOME PAGE"
+  activeControls() {
+    const btnNext = document.querySelector(".movies-btn--next");
+    const btnPrevious = document.querySelector(".movies-btn--previous");
+    btnPrevious.classList.add("hidden");
+
+    btnPrevious.addEventListener("click", this.previousAction).bind(this);
+
+    btnNext.addEventListener("click", this.nextAction).bind(this);
+  }
+
+  previousAction(e) {
+    e.preventDefault();
+    master.scrollIntoView({
+      behaviour: "smooth",
+    });
+
+    if (this.initialCount === 0 && this.totalCount == 8)
+      return btnPrevious.classList.add("hidden");
+
+    if (currentPage > 1 && this.initialCount === 0) {
+      currentPage--;
+      this.initialCount = maxNumberInPage - 4;
+      this.totalCount = maxNumberInPage + 1;
+      console.log(this.initialCount, this.totalCount);
+      return apiCall(urlAPI);
+    }
+
+    this.initialCount -= 5;
+    this.totalCount -= 5;
+    apiCall(urlAPI);
+  }
+
+  nextAction(e) {
+    e.preventDefault();
+    master.scrollIntoView({
+      behaviour: "smooth",
+    });
+
+    if (this.totalCount >= maxNumberInPage) {
+      currentPage++;
+
+      this.initialCount = -5;
+      this.totalCount = 0;
+    }
+    btnPrevious.classList.remove("hidden");
+    this.initialCount += 5;
+    this.totalCount += 5;
+    apiCall(urlAPI);
+  }
+
+  initDispaly() {
+    this.initialCount = 0;
+    this.totalCount = 5;
+    this.currentPage = 1;
+  }
+}
+
 /////////////////////
 // API CALLERS
 const trendingAPI = `https://api.themoviedb.org/3/trending/movie/day?language=en-US&page=${currentPage}`;
@@ -77,7 +281,7 @@ async function apiCall(url) {
     const data = await res.json();
     const { results } = data;
     maxNumberInPage = results.length - 1;
-    const result = results.slice(initialCount, totalCount);
+    const result = results.slice(this.initialCount, totalCount);
 
     DisplayTrending(result);
 
@@ -139,6 +343,7 @@ async function DisplaySearchOutput(movieName) {
 
   const searchAPI = `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=${currentPage}`;
   urlAPI = searchAPI;
+
   try {
     const res = await fetch(urlAPI, options);
     if (!res.ok) throw new Error();
